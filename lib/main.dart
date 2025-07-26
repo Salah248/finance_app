112d2cc25149c9c5c57b69540d67f9fd23a3449d
Salah248 <salahsadoon248@gmail.com>
2025-07-26 19:37:37 +0300

    Refactor app structure: integrate ScreenUtil for responsive design, implement routing, and add custom UI components

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:finance_app/resources/app_size.dart';
+import 'package:finance_app/resources/route_manager.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,58 +12,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    return ScreenUtilInit(
+      designSize: const Size(AppSize.screenWidth, AppSize.screenHeight),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) => MaterialApp.router(
+        title: 'Finance App',
+        theme: ThemeData(useMaterial3: true),
+        debugShowCheckedModeBanner: false,
+        routerConfig: RouteManager.router,
       ),
     );
   }
--- lib/resources/route_manager.dart
+++ lib/resources/route_manager.dart
@@ -1,1 +1,77 @@
+import 'package:finance_app/ui/screen/create_new_password_screen.dart';
+import 'package:finance_app/ui/screen/forgot_password_screen.dart';
+import 'package:finance_app/ui/screen/home_screen.dart';
+import 'package:finance_app/ui/screen/login_screen.dart';
+import 'package:finance_app/ui/screen/my_card_screen.dart';
+import 'package:finance_app/ui/screen/my_profile_screen.dart';
+import 'package:finance_app/ui/screen/on_boarding_screen.dart';
+import 'package:finance_app/ui/screen/otp_verifcation_screen.dart';
+import 'package:finance_app/ui/screen/password_changed_screen.dart';
+import 'package:finance_app/ui/screen/register_screen.dart';
+import 'package:finance_app/ui/screen/statistic_screen.dart';
+import 'package:go_router/go_router.dart';
 
+class Routes {
+  static const String onBoardingRoute = '/';
+  static const String loginRoute = '/login';
+  static const String registerRoute = '/register';
+  static const String forgotPasswordRoute = '/forgotPassword';
+  static const String createNewPasswordRoute = '/createNewPassword';
+  static const String passwordChangedRoute = '/passwordChanged';
+  static const String otpVerifcationRoute = '/otpVerifcation';
+  static const String homeRoute = '/home';
+  static const String myCardRoute = '/myCard';
+  static const String myProfileRoute = '/myProfile';
+  static const String statisticRoute = '/statistic';
+}
+
+class RouteManager {
+  static final GoRouter router = GoRouter(
+    routes: [
+      GoRoute(
+        path: Routes.onBoardingRoute,
+        builder: (context, state) => const OnBoardingScreen(),
+      ),
+      GoRoute(
+        path: Routes.loginRoute,
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: Routes.registerRoute,
+        builder: (context, state) => const RegisterScreen(),
+      ),
+      GoRoute(
+        path: Routes.forgotPasswordRoute,
+        builder: (context, state) => const ForgotPasswordScreen(),
+      ),
+      GoRoute(
+        path: Routes.otpVerifcationRoute,
+        builder: (context, state) => const OtpVerifcationScreen(),
+      ),
+      GoRoute(
+        path: Routes.createNewPasswordRoute,
+        builder: (context, state) => const CreateNewPasswordScreen(),
+      ),
+      GoRoute(
+        path: Routes.passwordChangedRoute,
+        builder: (context, state) => const PasswordChangedScreen(),
+      ),
+      GoRoute(
+        path: Routes.homeRoute,
+        builder: (context, state) => const HomeScreen(),
+      ),
+      GoRoute(
+        path: Routes.myCardRoute,
+        builder: (context, state) => const MyCardScreen(),
+      ),
+      GoRoute(
+        path: Routes.myProfileRoute,
+        builder: (context, state) => const MyProfileScreen(),
+      ),
+      GoRoute(
+        path: Routes.statisticRoute,
+        builder: (context, state) => const StatisticScreen(),
+      ),
+    ],
+  );
+}
